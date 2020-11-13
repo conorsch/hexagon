d65e8d60e27d097feb27a16304491fd9ed92ee98
Conor Schaefer <conor@freedom.press>
2020-11-13 13:01:52 -0800

    Builds RPM, attempt 4, reproducible!

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 lint:
 	black --line-length 100 --check .
 	flake8 --max-line-length 100
-	
+
 .PHONY: fmt
 fmt:
 	black --line-length 100 .
@@ -10,7 +10,6 @@ fmt:
 .PHONY: clone
 clone:
 	./scripts/clone-to-dom0
-
 .PHONY: test
 test:
 	PYTHONPATH=$$PWD pytest-3 -vv tests
@@ -21,7 +20,16 @@ watch:
 
 .PHONY: install-deps
 install-deps:
-	sudo apt install -y black flake8 python3-pytest qubes-core-admin-client inotify-hookable
+	sudo apt install -y black flake8 python3-pytest qubes-core-admin-client inotify-hookable \
+		rpm rpm-common diffoscope reprotest
+
+.PHONY: rpm
+rpm:
+	./scripts/build-dom0-rpm
+
+.PHONY: reprotest
+reprotest:
+	reprotest -c "make rpm" . "rpm-build/RPMS/noarch/*.rpm"
 
 .PHONY: install
 install:
---
+++ setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+
+exclude = ["tests"]
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+version = "0.1.0"
+
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name="hexagon",
+        author="Conor Schaefer",
+        version=version,
+        author_email="conor@freedom.press",
+        description="Experimental CLI for managing Qubes OS VMs",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        license="GPLv3+",
+        packages=setuptools.find_packages(exclude=exclude),
+        python_requires=">=3.5",
+        url="https://github.com/conorsch/hexagon",
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Programming Language :: Python :: 3",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+            "License :: OSI Approved :: " "GNU General Public License v3 or later (GPLv3+)",
+            "Intended Audience :: Developers",
+            "Operating System :: OS Independent",
+        ],
+        entry_points={"console_scripts": ["hexagon=hexagon.cli:main"]},
+    )
