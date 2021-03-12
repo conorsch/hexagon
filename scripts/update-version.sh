f4b238edb3c0cc4314177f22162c70542169a133
Conor Schaefer <conor@freedom.press>
2021-03-12 12:46:51 -0800

    Touches up build tooling
    Chore for updating the scripts to bump versions.

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -13,5 +13,15 @@ fi
 new_version="$1"
 shift
 
-sed -i "s/^version = .*$/version = \"$new_version\"/" setup.py
-sed -i "s/^%global version .*$/%global version $new_version/" rpm-build/SPECS/hexagon.spec
+spec_file="rpm-build/SPECS/hexagon.spec"
+
+sed -i "s/^version = .*$/version = \"${new_version}\"/" setup.py
+sed -i "s/^%global version .*$/%global version ${new_version}/" "$spec_file"
+perl -pi -e "s/hexagon-[\\d\\.]+-1.fc25.noarch.rpm/hexagon-${new_version}-1.fc25.noarch.rpm/" README.md
+
+echo "Updated to version $new_version"
+echo "Edit the spec file to add changelog: $spec_file"
+echo "Then run: "
+# Not automating the tag creation, because the changelog
+# still needs updates. =/
+printf '\n\tgit tag -a -s %s -m "hexagon %s"\n\n' "$new_version" "$new_version"
